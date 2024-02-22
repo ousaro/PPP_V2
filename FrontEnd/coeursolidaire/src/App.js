81849d22279070a0291b8c17cba1384d16d07f57
Osaro20 <oussamaouldrhila292@gmail.com>
2024-02-22 19:02:52 +0100

    add navbar

--- FrontEnd/coeursolidaire/src/App.js
+++ FrontEnd/coeursolidaire/src/App.js
@@ -1,9 +1,9 @@
-
+import NavBar from "./NavBar";
 
 function App() {
   return (
     <div className="App">
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro quae a consequatur beatae officiis id inventore numquam eius voluptate adipisci quibusdam, corporis earum deserunt fugit. Iure impedit deleniti tempora rem veritatis tempore eligendi sunt vel assumenda nisi voluptates at, ab accusantium nesciunt officia reprehenderit quas dolores neque suscipit quisquam et.</p>
+      <NavBar></NavBar>
     </div>
   );
 }

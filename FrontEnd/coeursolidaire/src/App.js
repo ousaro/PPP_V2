88b888565c8edc8cc87d8b215e326ded1bc550fe
Osaro20 <oussamaouldrhila292@gmail.com>
2024-02-24 19:17:49 +0100

    adding the home page

--- FrontEnd/coeursolidaire/src/App.js
+++ FrontEnd/coeursolidaire/src/App.js
@@ -1,20 +1,15 @@
-<<<<<<< HEAD
+
 import Home from "./Pages/Home"; 
 import {BrowserRouter, Route, Switch} from "react-router-dom"
-=======
->>>>>>> 9bfac1e7283a4986aeb80c1a60d64226c39233cd
 
 
 function App() {
   return (
     <BrowserRouter>
     <div className="App">
-<<<<<<< HEAD
       <Switch>
         <Route path="/" exact><Home></Home></Route>
       </Switch>
-=======
->>>>>>> 9bfac1e7283a4986aeb80c1a60d64226c39233cd
       
     </div>
     </BrowserRouter>

9d1727ef3a5cb2279ddaf138c86e90be99387a10
Osaro20 <oussamaouldrhila292@gmail.com>
2024-02-27 23:37:38 +0100

    adding more details to the log in pg

--- FrontEnd/coeursolidaire/src/Pages/LogIn.js
+++ FrontEnd/coeursolidaire/src/Pages/LogIn.js
@@ -1,3 +1,9 @@
+import { Link } from "react-router-dom"
+import logo from "../imgs/HomeLogo.png"
+import igIcon from "../imgs/InstagramIcon.png"
+import fbIcon from "../imgs/facebookIcon.png"
+import inIcon from "../imgs/LinkdinIcon.png"
+
 
 
 const LogIn = () => {
@@ -7,8 +13,52 @@ const LogIn = () => {
             <main className="LogIn_Content">
 
                 <form className="LogIn_Content_Form">
-                    <input type="text"/>
 
+                    <section className="LogIn_Form_Header">
+                        <h1>Log In</h1>
+                    </section>
+
+                    <section className="LogIn_Form_Content">
+
+                        <p>Please fill your information below</p>
+
+                        <input type="text" placeholder="Email" />
+                        <input type="password" placeholder="Password"/>
+
+                        <p><Link>Forgot Password?</Link></p>
+
+                        <section className="LogIn_Form_Terms">
+                            <input type="radio" />
+                            <p>I accept terms & privacy policy</p>
+                        </section>
+                        
+
+                        <p>Not a member? <Link>SignIn</Link></p>
+
+                        <Link><button className="LogIn_Form_Btn">Log In</button></Link>
+
+                    </section>
+                    
+
+                    <section className="LogIn_Form_Footer">
+
+                        <figure className="LogIn_Form_Logo">
+                            <img src={logo} alt="Logo" width="109" height="105"/>
+                        </figure>
+
+                        <div className="LogIn_Form_SocialLinks">
+                            <ul>
+                                <li><a href="/"  rel="noopener noreferrer"><img src={inIcon} alt="Linkdin" width="40" height="38" /></a></li>
+                                <li><a href="/"  rel="noopener noreferrer"><img src={fbIcon} alt="Facebook" width="40" height="38"/></a></li>
+                                <li><a href="/"  rel="noopener noreferrer"><img src={igIcon} alt="Instagram" width="40" height="38"/></a></li>
+                            </ul>
+                        </div>
+
+
+
+                    </section>
+
+                    
                 </form>
 
             </main>

d8bd4cd3a57f7411181ffa1b4d671452d47acdb1
Osaro20 <oussamaouldrhila292@gmail.com>
2024-03-03 14:50:58 +0100

    fixing some problems

--- FrontEnd/coeursolidaire/src/Pages/Users Home pages/VuserHome.js
+++ FrontEnd/coeursolidaire/src/Pages/Users Home pages/VuserHome.js
@@ -39,6 +39,9 @@ const VuserHome = () => {
                                 <p>Association name</p>
                                 <p>not verified</p>
                             </div>
+                            <div className="Post_Header_Date">
+                                <p>10 min ago</p>
+                            </div>
                         </div>
                        
                        <div className="Post_Content">
@@ -62,132 +65,6 @@ const VuserHome = () => {
 
 
                     </div>
-
-
-                    <div className="UserHome_Post">
-
-<div className="Post_Header">
-    <div className="Nav_ProfileIcon Post_Header_ProfileIcon"></div>
-    <div className="Post_Header_Info">
-        <p>Association name</p>
-        <p>not verified</p>
-    </div>
-</div>
-
-<div className="Post_Content">
-
-    
-    <div className="Post_Content_Description">
-        <p>Nous sommes à la recherche de bénévoles passionnés et engagés pour participer à notre programme de cours de soutien. 
-        </p>
-    </div>
-    
-
-    <figure className="Post_Content_Image">
-        <img src={TestImg} alt="Post_image" width="641" height="341" />
-    </figure>
-    
-    
-
-</div>
-
-<button className="Post_btn">Participate</button>
-
-
-</div>
-<div className="UserHome_Post">
-
-<div className="Post_Header">
-    <div className="Nav_ProfileIcon Post_Header_ProfileIcon"></div>
-    <div className="Post_Header_Info">
-        <p>Association name</p>
-        <p>not verified</p>
-    </div>
-</div>
-
-<div className="Post_Content">
-
-    
-    <div className="Post_Content_Description">
-        <p>Nous sommes à la recherche de bénévoles passionnés et engagés pour participer à notre programme de cours de soutien. 
-        </p>
-    </div>
-    
-
-    <figure className="Post_Content_Image">
-        <img src={TestImg} alt="Post_image" width="641" height="341" />
-    </figure>
-    
-    
-
-</div>
-
-<button className="Post_btn">Participate</button>
-
-
-</div>
-<div className="UserHome_Post">
-
-<div className="Post_Header">
-    <div className="Nav_ProfileIcon Post_Header_ProfileIcon"></div>
-    <div className="Post_Header_Info">
-        <p>Association name</p>
-        <p>not verified</p>
-    </div>
-</div>
-
-<div className="Post_Content">
-
-    
-    <div className="Post_Content_Description">
-        <p>Nous sommes à la recherche de bénévoles passionnés et engagés pour participer à notre programme de cours de soutien. 
-        </p>
-    </div>
-    
-
-    <figure className="Post_Content_Image">
-        <img src={TestImg} alt="Post_image" width="641" height="341" />
-    </figure>
-    
-    
-
-</div>
-
-<button className="Post_btn">Participate</button>
-
-
-</div>
-<div className="UserHome_Post">
-
-<div className="Post_Header">
-    <div className="Nav_ProfileIcon Post_Header_ProfileIcon"></div>
-    <div className="Post_Header_Info">
-        <p>Association name</p>
-        <p>not verified</p>
-    </div>
-</div>
-
-<div className="Post_Content">
-
-    
-    <div className="Post_Content_Description">
-        <p>Nous sommes à la recherche de bénévoles passionnés et engagés pour participer à notre programme de cours de soutien. 
-        </p>
-    </div>
-    
-
-    <figure className="Post_Content_Image">
-        <img src={TestImg} alt="Post_image" width="641" height="341" />
-    </figure>
-    
-    
-
-</div>
-
-<button className="Post_btn">Participate</button>
-
-
-</div>
 
                 </section>
                 
